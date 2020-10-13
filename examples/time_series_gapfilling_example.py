524d25b55c8b3a850fe0f12b0bd9d8ccdce2e0a1
Dreamlone <mik_sar@mail.ru>
2020-10-13 11:20:59 +0300

    fix names

--- examples/time_series_gapfilling_example.py
+++ examples/time_series_gapfilling_example.py
@@ -35,12 +35,12 @@ if __name__ == '__main__':
 
     # Filling in gaps
     gapfiller = AdvancedGapfiller(gap_value=-100.0)
-    withoutgap_arr = gapfiller.inverse_ridge(tmp_data, max_window_size=400)
+    without_gap_arr = gapfiller.inverse_ridge(tmp_data, max_window_size=400)
 
     simple_gapfill = SimpleGapfiller(gap_value=-100.0)
-    withoutgap_arr_poly = simple_gapfill.local_poly_approximation(tmp_data, 4, 150)
+    without_gap_arr_poly = simple_gapfill.local_poly_approximation(tmp_data, 4, 150)
 
-    plt.plot(withoutgap_arr, c='blue', alpha=0.5)
-    plt.plot(withoutgap_arr_poly, c='orange', alpha=0.4)
+    plt.plot(without_gap_arr, c='blue', alpha=0.5)
+    plt.plot(without_gap_arr_poly, c='orange', alpha=0.4)
     plt.grid()
     plt.show()
